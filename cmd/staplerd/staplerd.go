cdf83cb70c9a8bc1910929af704f7b987976c0c1
Toru Maesaka <toru@torumk.com>
2017-04-01 21:19:14 -0700

    Add the flag package for command-line options
    Starting with the port option.
    
    Resolves #2

--- cmd/staplerd/staplerd.go
+++ cmd/staplerd/staplerd.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func usage() {
 	fmt.Printf("staplerd | the stapler daemon\n")
+	fmt.Printf("  --port <num>  %s\n", flag.Lookup("port").Usage)
 	os.Exit(0)
 }
 
 func main() {
-	usage()
+	var port int
+
+	flag.IntVar(&port, "port", 8084, "TCP port number to listen on (default: 8084)")
+
+	fmt.Printf("listening on port: %d\n", port)
 }
