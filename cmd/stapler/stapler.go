ee3b8274d7da9f5236b33722736f065be8754c13
Toru Maesaka <toru@torumk.com>
2017-04-04 22:26:01 -0700

    Accept a map option from the command line
    Resolves #3

--- cmd/stapler/stapler.go
+++ cmd/stapler/stapler.go
@@ -10,14 +10,22 @@ import (
 
 func usage() {
 	fmt.Printf("stapler server\n")
-	fmt.Printf("  --port <num>  %s\n", flag.Lookup("port").Usage)
+	fmt.Printf("  --map  <path>  %s\n", flag.Lookup("map").Usage)
+	fmt.Printf("  --port <num>   %s\n", flag.Lookup("port").Usage)
+
 	os.Exit(0)
 }
 
 func main() {
 	config := config.NewConfig()
 
+	flag.StringVar(&config.MapPath, "map", "", "Path to the map file (required)")
 	flag.IntVar(&config.Port, "port", 8084, "TCP port number to listen on (default: 8084)")
+	flag.Parse()
+
+	if len(config.MapPath) == 0 {
+		usage()
+	}
 
 	fmt.Printf("listening on port: %d\n", config.Port)
 }
--- config/config.go
+++ config/config.go
@@ -2,7 +2,8 @@ package config
 
 // Config holds the stapler server settings.
 type Config struct {
-	Port int
+	MapPath string // Path to the route definition file
+	Port    int    // TCP port number to listen on
 }
 
 // NewConfig returns a new Config.
