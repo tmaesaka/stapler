92b48c24180dee8e3b3097de2bdf257539faf710
Toru Maesaka <toru@torumk.com>
2017-04-14 07:10:36 -0700

    Some boilerplate to get started with loading the map

--- app/server.go
+++ app/server.go
@@ -1,12 +1,38 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/tmaesaka/stapler/config"
 )
 
+// Temporarily unmarshal the map config into this map.
+type StaplerMap struct {
+	Routes []map[string]interface{}
+}
+
 func Run(cfg *config.Config) error {
+	var (
+		mapContent []byte
+		err        error
+		staplerMap StaplerMap
+	)
+
+	if mapContent, err = ioutil.ReadFile(cfg.MapPath); err != nil {
+		log.Fatalf("Failed to open: %s", cfg.MapPath)
+	}
+
+	if err = json.Unmarshal(mapContent, &staplerMap); err != nil {
+		log.Fatalf("Failed to load: %s %v", cfg.MapPath, err)
+	}
+
+	for _, route := range staplerMap.Routes {
+		fmt.Printf("%s => %s\n", route["path"], route["file"])
+	}
+
 	fmt.Printf("listening on port: %d\n", cfg.Port)
 	return nil
 }
